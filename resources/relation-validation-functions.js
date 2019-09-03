c094db293038a9813d397df20095206aa8e4f30d
youngtai <youngtaiahn@gmail.com>
2019-09-02 19:10:14 -0600

    Incremental: Refactored "Has Title" to do things the new way.
    Still can't figure out why the "bad" link style doesn't change.

--- resources/relation-validation-functions.js
+++ resources/relation-validation-functions.js
@@ -1,35 +1,77 @@
 function buildDisplayedGraph(relationType) {
-  var relations = displayedGraph.relations;
-  // remove irrelevant relationships from displayedGraph.relations
-  for (var i = relations.length - 1; i>= 0; i--) {
-    if (relations[i].type != relationType) {
-      relations.splice(i,1);
+  function getDisplayRelations() {
+    var relations = displayedGraph.relations;
+    // remove irrelevant relationships from displayedGraph.relations
+    for (var i = relations.length - 1; i>= 0; i--) {
+      if (relations[i].type != relationType) {
+        relations.splice(i,1);
+      }
     }
-  }
-  // add relevant relations to displayedGraph.relations from editedGraph.relations
-  for (i in editedGraph.relations) {
-    var relation = editedGraph.relations[i];
-    if (!relationInGraph(relation) && (relation.type == relationType) && !/S=\d\+|S=\d/.test(relation.type)) {
-      relations.push({type : relation.type, 
-                            startToken : relation.startToken, 
-                            source : relation.source, 
-                            endToken : relation.endToken, 
-                            target : relation.target,
-													distance : linkDistance(relation.type)})
+    // add relevant relations to displayedGraph.relations from editedGraph.relations
+    for (i in editedGraph.relations) {
+      let relation = editedGraph.relations[i];
+      if (!relationInGraph(relation) && (relation.type == relationType) && !/S=\d\+|S=\d/.test(relation.type)) {
+        relations.push({type : relation.type, 
+                              startToken : relation.startToken, 
+                              source : relation.source, 
+                              endToken : relation.endToken, 
+                              target : relation.target,
+                            distance : linkDistance(relation.type)})
+      }
     }
+    return relations;
   }
-  // return relations;
-  displayedGraph.relations = relations;
-  displayedGraph.entities = function getDisplayEntities() {
+  function getDisplayEntities() {
     var entitySet = new Set();
-    for (relation of displayedGraph.relations) {
-      entitySet.add(relation.source);
-      entitySet.add(relation.target);
-    }
-    console.log(entitySet);
-    console.log(Array.from(entitySet));
+    var entityArray = [];
+    // Get entities of correct relation type from displayedGraph
+    displayedGraph.relations.forEach(function(relation) {
+      if (relation.type == relationType) {
+        entitySet.add(relation.source);
+        entitySet.add(relation.target);
+      }
+    });
+    // Add in entities of correct relation type from editedGraph
+    editedGraph.relations.forEach(function(relation) {
+      if (relation.type == relationType) {
+        // Add the source and target (have to get them from the editedGraph.entities)
+        let source = editedGraph.getEntityById(relation.source);
+        let target = editedGraph.getEntityById(relation.target);
+
+        function entityInSet(e) {
+          for (let entity of entitySet) {
+            if (entity.id == e.id) {
+              return true;
+            }
+          }
+          return false;
+        }
+        // If it's already in the set, we don't add it
+        // We can't rely on the set determining uniqueness for us because
+        // we are getting different objects that represent the same thing
+        // an entity from editedGraph.entity and from displayedGraph.entity
+        if (!entityInSet(source)) {
+          entitySet.add(source);
+        }
+        if (!entityInSet(target)) {
+          entitySet.add(target);
+        }
+
+      }
+    });
+    // console.log(entitySet);
+    // console.log(displayedGraph.entities);
+    // console.log(Array.from(entitySet));
+    // for (let entity of displayedGraph.entities) {
+    //   if (entitySet.has(entity)) {
+    //     entityArray.push(entity);
+    //   }
+    // }
+    // return entityArray;
     return Array.from(entitySet);
   }
+  displayedGraph.entities = getDisplayEntities();
+  displayedGraph.relations = getDisplayRelations();
 }
 
 function checkHasTitle() {
@@ -87,7 +129,7 @@ function checkHasTitle() {
     "WORK_OF_ART"
   ];
   var badRelations = [];
-  for (let relation of displayedGraph.relations) {
+  displayedGraph.relations.forEach(function(relation) {
     // check source and target against all types, for each relation
     for (let type of types) {
       if (relation.source.type == type) {
@@ -99,10 +141,10 @@ function checkHasTitle() {
         console.log("Found a bad relation with bad target.");
       }
     }
-  }
+  });
   
   console.log("Checked 'Has Title' relation");
-  restart(badRelations);
+  restart();
   // Now we change the styling of the bad entity nodes and bad rel links.
   
   // Styling for nodes/links of bad entities/relations
